c5ecb8ed5671ac5e4f8010d458eb40c9fd0b65e0
Tiago da Silva <64290445+tdsh-ux@users.noreply.github.com>
2020-07-31 05:58:18 -0300

    Update heatmap_clustering.R

--- Part I/heatmap_clustering.R
+++ Part I/heatmap_clustering.R
@@ -1,13 +1,15 @@
 # Hierarchical clustering
 library(tidyverse)
+
 set.seed(1234)
 par(mar = c(5,5,5,5))
+
 x <- rnorm(12, mean = rep(1:3, each = 4), sd = .2)
 y <- rnorm(12, mean = rep(c(1, 2, 1), each = 4), sd = .2)
 plot(x, y, col = "blue", pch = 19, cex = 2)
+
 text(x + .05, y +.05, labels = as.character(1:12))
 df <- data.frame(x = x, y = y)
-
 dist(df)   # gives the distance matrix
 
 # choose the closer points; they form a cluster.
